8e3de3c3c619a9ee5277dd79152bba3d6e2bcb44
kidots77 <baka271197@mail.ru>
2023-11-26 16:17:35 +0300

    pictures fix

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -128,7 +128,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "collected_static"
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
